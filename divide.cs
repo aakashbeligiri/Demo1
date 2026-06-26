2cdd2bc924c5d6172826513cc2e08a077a5ab28e
PRAVALLIKA <PRAVALLIKA@DESKTOP-JV3JTH1>
2026-06-26 14:13:28 -0400

    divide

---
+++ divide.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double num1 = 10;
+        double num2 = 3;
+
+        double result = num1 / num2;
+
+        Console.WriteLine("Result: " + result);
+    }
+}
